de7f03ee520cc4855bbf22660b1bc62bc94be0d2
Sewmieman <yosef123@gmail.com>
2026-09-17 18:50:35 +0300

    flutter

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,97 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decreaseCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+
+      // Bottom-right + and - buttons
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // Minus button
+          IconButton(
+            onPressed: _decreaseCounter,
+            icon: const Icon(Icons.remove),
+            iconSize: 35,
+          ),
+
+          const SizedBox(width: 10),
+
+          // Plus button
+          IconButton(
+            onPressed: _incrementCounter,
+            icon: const Icon(Icons.add),
+            iconSize: 35,
+          ),
+        ],
+      ),
+    );
+  }
+}
